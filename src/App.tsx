a54571590f7ca4d0e95f3f66f85fe96f7aa33c3c
Vitor Silva <vitor.silva@ntendencia.com.br>
2021-07-20 14:39:11 -0300

    Creating register function

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
-import React, { useState } from 'react';
-import TagsInput from './TagsInput';
+import React, { useState } from 'react'
+import TagsInput from './components/TagsInput'
+
+interface keyPressObj {
+  code: string
+  target: { value: string }
+  preventDefault: Function
+}
 
 const App: React.FC = () => {
-  function handleSelecetedTags(items: string[]) {
-    return items;
+  /* Object used to serve as a typed character comparator in function insertInputValue, with the following summary:
+   * NumpadEnter: Enter key pressed located in the number section of keyboard
+   * Enter: Enter key pressed located in the characters section of keyboard
+   * Tab: Tab key pressed
+   * Slash: ';' key pressed
+   * Space: Space bar pressed
+   */
+  const registerCharacters: object = {
+    0: 'NumpadEnter',
+    1: 'Tab',
+    3: 'Slash',
+    4: 'Space',
+    5: 'Enter',
   }
 
+  // Function that check all key pressed in input and record a new tag if the key pressed is any of the key in the onject registerCharacters
+  const insertInputValue = (e: keyPressObj) => {
+    for (let i in registerCharacters) {
+      if (e.code === registerCharacters[i]) {
+        e.preventDefault()
+        setTags([...tags, e.target.value])
+      }
+    }
+  }
+
+  // State variable that store all tags
   const [tags, setTags] = useState<string[]>([
-    "contato@rarolabs.com.br",
-    "nao-responda@rarolabs.com.br"
-  ]);
+    'contato@rarolabs.com.br',
+    'nao-responda@rarolabs.com.br',
+  ])
 
   return (
     <div className="App">
       <TagsInput
-        onSelectTags={handleSelecetedTags}
+        onKeyDown={(e: keyPressObj) => insertInputValue(e)}
         fullWidth
         variant="outlined"
         id="tags"
@@ -24,7 +52,7 @@ const App: React.FC = () => {
         tags={tags}
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
